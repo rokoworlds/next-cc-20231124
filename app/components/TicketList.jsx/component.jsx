657dbca04d9d6d1d1d836f6e501bd38ceaf540d7
Roman Coropat <roman.koropat@gmail.com>
2023-11-25 18:11:46 +0200

    add: loading page and ticket page suspense

--- app/components/TicketList.jsx/component.jsx
+++ app/components/TicketList.jsx/component.jsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 
 async function getTickets() {
+
+    await new Promise(resolve => setTimeout(resolve, 3000)) // delete after test
+
     const response = await fetch('http://localhost:4000/tickets');
 
     return response.json()
